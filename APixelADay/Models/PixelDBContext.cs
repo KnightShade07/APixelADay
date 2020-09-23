b30af778fb4ce3cc604c5148a7e6662bea926013
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-09-22 20:08:42 -0700

    Issue#6 setup db (#14)
    * Added PixelDBContext Class, Add DbSet variable.
    
    * Set up DbConnection in appsettings and Startup
    
    * Added Code First Migration

---
+++ APixelADay/Models/PixelDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APixelADay.Models
+{
+    public class PixelDBContext : DbContext
+    {
+        public PixelDBContext(DbContextOptions<PixelDBContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<PixelArt> PixelArts { get; set; }
+    }
+}
