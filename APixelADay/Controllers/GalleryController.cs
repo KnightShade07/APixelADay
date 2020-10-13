147386dbf318bad8e33afc27bfed79df93008a2c
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-10-12 20:27:48 -0700

    Added Post Version of Add in the Gallery Controller

--- APixelADay/Controllers/GalleryController.cs
+++ APixelADay/Controllers/GalleryController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using APixelADay.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APixelADay.Controllers
@@ -17,6 +18,14 @@ namespace APixelADay.Controllers
         public IActionResult Add()
         {
            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult AddPixel()
+        {
+            //redirect back to Gallery Page
+            return RedirectToAction("Gallery");
         }
     }
 }
