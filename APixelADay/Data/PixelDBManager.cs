242da755a7f647451917787f4a326985b2313474
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-10-12 20:58:54 -0700

    Created Data Folder, Added PixelDBManager Class.

---
+++ APixelADay/Data/PixelDBManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APixelADay.Data
+{
+    /// <summary>
+    /// This class is responsible for managing Database
+    /// transactions with the PixelArt Database
+    /// In order to keep as much database code out
+    /// of the controllers as possible.
+    /// </summary>
+    public class PixelDBManager
+    {
+    }
+}
