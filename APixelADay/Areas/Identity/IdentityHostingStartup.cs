eb7a341807dfa1d40ee9344be3b186f0a481d0f2
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-11-18 16:01:26 -0800

    Added ASP.NET Identity (#100)
    * Added ASP.NET Identity
    
    * Updated .netcore version from 3.1 to 5.0 (#101)

---
+++ APixelADay/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using APixelADay.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(APixelADay.Areas.Identity.IdentityHostingStartup))]
+namespace APixelADay.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<APixelADayContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("APixelADayContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<APixelADayContext>();
+            });
+        }
+    }
+}
