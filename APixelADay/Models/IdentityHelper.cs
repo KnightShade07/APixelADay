0ae3d12ca23d51ab4c876388b653eaa9e1ae34f0
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-11-21 15:36:01 -0800

    Added User and Administrator roles

--- APixelADay/Models/IdentityHelper.cs
+++ APixelADay/Models/IdentityHelper.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,8 +8,11 @@ using System.Threading.Tasks;
 
 namespace APixelADay.Models
 {
-    public static class IdentityHelper
+    public class IdentityHelper
     {
+        //role names
+        public const string Administrator = "Administrator";
+        public const string User = "User";
         public static void SetIdentityOptions(IdentityOptions options)
         {
             //setting sign in options.
@@ -22,5 +27,21 @@ namespace APixelADay.Models
             options.Password.RequireNonAlphanumeric = false;
 
         }
+
+        public static async Task CreateRoles(IServiceProvider provider, params string[] roles)
+        {
+            RoleManager<IdentityRole> roleManager = provider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            //create role if it does not exist.
+            foreach (string role in roles)
+            {
+                bool doesRoleExist = await roleManager.RoleExistsAsync(role);
+                if (!doesRoleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+        
     }
 }
