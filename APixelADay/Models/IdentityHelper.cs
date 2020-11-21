f18742a6340b00a1f13b066871f3dcea08452bff
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-11-21 11:17:37 -0800

    Added IdentityHelper Class

---
+++ APixelADay/Models/IdentityHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APixelADay.Models
+{
+    public static class IdentityHelper
+    {
+        public static void SetIdentityOptions(IdentityOptions options)
+        {
+            //setting sign in options.
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+
+            //sets password strength.
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 10;
+            options.Password.RequireNonAlphanumeric = false;
+
+        }
+    }
+}
