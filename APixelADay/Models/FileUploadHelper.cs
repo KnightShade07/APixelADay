ecc97f6524277e6b7cd5653af46623f07f3e0973
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-11-27 15:52:26 -0800

    Added IsFileEmpty and IsValidExtension Methods

--- APixelADay/Models/FileUploadHelper.cs
+++ APixelADay/Models/FileUploadHelper.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,29 @@ namespace APixelADay.Models
 {
     public static class FileUploadHelper
     {
+         public enum FileTypes
+        {
+            Photo
+        }
+        public static bool IsFileEmpty(IFormFile file)
+        {
+            if(file.Length == 0)
+            {
+                return true;
+            }
+            return false;
+        }
 
+        public static bool IsValidExtension(IFormFile file, FileTypes type)
+        {
+            string extension = Path.GetExtension(file.FileName).ToLower();
+            string[] photoExtensions = { ".png", ".gif" };
+            if (photoExtensions.Contains(extension))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
