48506cef96b25deec495c0b6d92a88c570fac255
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-11-27 16:04:38 -0800

    Created class, added UploadBlob Method

---
+++ APixelADay/Models/BlobStorageHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APixelADay.Models
+{
+    public class BlobStorageHelper
+    {
+        private async Task<FileStream> UploadBlob(PixelArt p, IFormFile Pixel)
+        {
+            string con = _config.GetSection("BlobStorageString").Value;
+
+            BlobServiceClient blobService = new BlobServiceClient(con);
+
+            //creates container to hold BLOBs.
+            BlobContainerClient containerClient = blobService.GetBlobContainerClient("PixelArts");
+            //makes sure container exists
+            if (!containerClient.Exists())
+            {
+                await containerClient.CreateAsync();
+                await containerClient.SetAccessPolicyAsync(PublicAccessType.Blob);
+            }
+
+
+
+
+            //Add BLOB to container.
+            string newfileName = Guid.NewGuid().ToString() + Path.GetExtension(Pixel.FileName);
+            BlobClient blobClient = containerClient.GetBlobClient(newfileName);
+            FileStream fileStream = System.IO.File.OpenRead("");
+            await blobClient.UploadAsync(p.PixelArtPhoto.OpenReadStream());
+            return fileStream;
+        }
+    }
+}
