89cabdb4f7a2a57bb7945ea15fdf2894d2af9739
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-11-27 16:04:20 -0800

    Moved BLOB Code out of GalleryController

--- APixelADay/Controllers/GalleryController.cs
+++ APixelADay/Controllers/GalleryController.cs
@@ -85,31 +85,7 @@ namespace APixelADay.Controllers
             return RedirectToAction("Gallery");
         }
 
-        private async Task<FileStream> UploadBlob(PixelArt p, IFormFile Pixel)
-        {
-            string con = _config.GetSection("BlobStorageString").Value;
-
-            BlobServiceClient blobService = new BlobServiceClient(con);
-
-            //creates container to hold BLOBs.
-            BlobContainerClient containerClient = blobService.GetBlobContainerClient("PixelArts");
-            //makes sure container exists
-            if (!containerClient.Exists())
-            {
-                await containerClient.CreateAsync();
-                await containerClient.SetAccessPolicyAsync(PublicAccessType.Blob);
-            }
-
-
-
-
-            //Add BLOB to container.
-            string newfileName = Guid.NewGuid().ToString() + Path.GetExtension(Pixel.FileName);
-            BlobClient blobClient = containerClient.GetBlobClient(newfileName);
-            FileStream fileStream = System.IO.File.OpenRead("");
-            await blobClient.UploadAsync(p.PixelArtPhoto.OpenReadStream());
-            return fileStream;
-        }
+        
 
         public  async Task <IActionResult> Edit(int id)
         {
