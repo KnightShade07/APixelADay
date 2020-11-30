c2c3d8ab40811b0f52a53fa48642b6a29f99bf9d
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-11-30 15:14:15 -0800

    Updated Migration

--- APixelADay/Migrations/20201130231259_secondMigration.Designer.cs
+++ APixelADay/Migrations/20201130231259_secondMigration.Designer.cs
@@ -4,14 +4,16 @@ using APixelADay.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace APixelADay.Migrations
 {
     [DbContext(typeof(PixelDBContext))]
-    partial class PixelDBContextModelSnapshot : ModelSnapshot
+    [Migration("20201130231259_secondMigration")]
+    partial class secondMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ APixelADay/Migrations/20201130231259_secondMigration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APixelADay.Migrations
+{
+    public partial class secondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "PixelArts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "PixelArts",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+    }
+}
