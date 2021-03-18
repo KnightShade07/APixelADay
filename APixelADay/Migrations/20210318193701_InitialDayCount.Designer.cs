ac066905c37a2ca397ca9d08e45b7950aa50c2dc
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2021-03-18 12:37:49 -0700

    Updated Migration to include DayCount.

--- APixelADay/Migrations/20210318193701_InitialDayCount.Designer.cs
+++ APixelADay/Migrations/20210318193701_InitialDayCount.Designer.cs
@@ -4,14 +4,16 @@ using APixelADay.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace APixelADay.Migrations
 {
     [DbContext(typeof(PixelDBContext))]
-    partial class PixelDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210318193701_InitialDayCount")]
+    partial class InitialDayCount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ APixelADay/Migrations/20210318193701_InitialDayCount.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APixelADay.Migrations
+{
+    public partial class InitialDayCount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DayCount",
+                table: "PixelArts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DayCount",
+                table: "PixelArts");
+        }
+    }
+}
