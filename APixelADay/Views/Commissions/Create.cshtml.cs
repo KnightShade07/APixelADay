889429a37cdbd30d88f34dfe5ec36f807302232a
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2021-06-21 20:47:48 -0700

    Added CRUD Pages for Comissions Log

---
+++ APixelADay/Views/Commissions/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using APixelADay.Models;
+
+namespace APixelADay.Views.Commissions
+{
+    public class CreateModel : PageModel
+    {
+        private readonly APixelADay.Models.PixelDBContext _context;
+
+        public CreateModel(APixelADay.Models.PixelDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public CommissionsLog CommissionsLog { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Commissions.Add(CommissionsLog);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ APixelADay/Views/Commissions/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using APixelADay.Models;
+
+namespace APixelADay.Views.Commissions
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly APixelADay.Models.PixelDBContext _context;
+
+        public DeleteModel(APixelADay.Models.PixelDBContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public CommissionsLog CommissionsLog { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            CommissionsLog = await _context.Commissions.FirstOrDefaultAsync(m => m.ComissionID == id);
+
+            if (CommissionsLog == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            CommissionsLog = await _context.Commissions.FindAsync(id);
+
+            if (CommissionsLog != null)
+            {
+                _context.Commissions.Remove(CommissionsLog);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
