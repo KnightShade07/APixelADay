89b4fabf4c430f4fd4034142d7b41da645c277ad
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2021-06-21 18:26:35 -0700

    Created Comissions Classs (#207)

---
+++ APixelADay/Models/Comissions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APixelADay.Models
+{
+    public class Comissions
+    {
+        [Key]
+        public int ComissionID { get; set; }
+        public string Title { get; set; }
+        [DisplayName("Price: ")]
+        public decimal Price { get; set; }
+        [DisplayName("Completion Status: ")]
+        public bool CompletionStatus { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayName("Date Started: ")]
+        public DateTime DateStarted { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Date Completed: ")]
+        public DateTime DateCompleted { get; set; }
+    }
+}
