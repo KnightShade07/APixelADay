73ce35183275a01fd409837617e34d1442f7fb5a
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2021-07-01 12:10:31 -0700

    Added GetAllCommissions async method

--- APixelADay/Data/PixelDBManager.cs
+++ APixelADay/Data/PixelDBManager.cs
@@ -45,6 +45,12 @@ namespace APixelADay.Data
                           select pixel).SingleAsync(); //gets a single item from the database
             
         }
+        //Gets all commissions from the database
+        public async static Task<int> GetTotalCommissionsAsync(PixelDBContext _context)
+        {
+            return await (from c in _context.Commissions
+                          select c).CountAsync();
+        }
 
 
     }
