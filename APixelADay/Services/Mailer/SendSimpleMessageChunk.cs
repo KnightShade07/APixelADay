12cd590b80ae5308f1dc30cdb77fe4dece6d9dc4
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2021-07-10 12:12:22 -0700

    Issue#139 initial mailgun integration (#275)
    * Created Services Folder
    
    * Added Mailer Folder
    
    * Created SendSimpleMessageChunk Testing Class
    
    * Added Restsharp usings
    
    * Added RestSharp Package
    
    * Added Class Code
    
    * Removed Main function

---
+++ APixelADay/Services/Mailer/SendSimpleMessageChunk.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace APixelADay.Services.Mailer
+{
+    public class SendSimpleMessageChunk
+    {
+        public static IRestResponse SendSimpleMessage()
+        {
+            RestClient client = new RestClient();
+            client.BaseUrl = new Uri("https://api.mailgun.net/v3");
+            client.Authenticator =
+                new HttpBasicAuthenticator("api",
+                                            "YOUR_API_KEY");
+            RestRequest request = new RestRequest();
+            request.AddParameter("domain", "YOUR_DOMAIN_NAME", ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", "Excited User <mailgun@YOUR_DOMAIN_NAME>");
+            request.AddParameter("to", "bar@example.com");
+            request.AddParameter("to", "YOU@YOUR_DOMAIN_NAME");
+            request.AddParameter("subject", "Hello");
+            request.AddParameter("text", "Testing some Mailgun awesomness!");
+            request.Method = Method.POST;
+            return client.Execute(request);
+        }
+    }
+}
